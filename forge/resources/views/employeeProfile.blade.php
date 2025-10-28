27cc0af3d0b0220ad8acb0e849e82f1a987d99fe
Brittany Fetters <fettsb24@gmail.com>
2025-10-28 15:42:58 -0400

    test push

---
+++ forge/resources/views/employeeProfile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="eprofile.css">
+    </head>
+
+    <body>
+        <div class="profile-box">
+            <h1>help</h1>
+        </div>    
+    </body>
+</html>
