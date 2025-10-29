ce494a885ad904fc21098cef10358b83fe78cd74
iallison0231@stevenscollege.edu <isaacallison562@gmail.com>
2025-10-29 13:47:20 -0400

    Started to work on form, I needed controller first to start.

--- forge/resources/views/employeeCreation.blade.php
+++ forge/resources/views/employeeCreation.blade.php
@@ -1,3 +1,12 @@
  <!-- // Evin Camacho -->
-  <form>
+  <html>
+    <body>
+  <form name='EvinCamacho' method='post'>
+    @csrf
+            employeeID:<input type='text' name='employeeID'> </input>
+                <br>
+        <button type='submit'>Create Employee</button>
+
 </form>
+</body>
+</html>
