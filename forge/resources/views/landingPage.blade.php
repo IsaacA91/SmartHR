cf091a87ffd915f6a613d220b7c7fd9faaf0e0e9
iallison0231@stevenscollege.edu <isaacallison562@gmail.com>
2025-10-31 12:37:58 -0400

    I created two forms, I hope this helps out

--- forge/resources/views/landingPage.blade.php
+++ forge/resources/views/landingPage.blade.php
@@ -4,8 +4,22 @@
         Welcome to Smart HR
 </h1>
 
+<form action="" method="post">
+    User Name: <input type="text"  name="username" required>
+    <br>
+    Password: <input type="text" name="password" required>
+    <br>
+    </form>
 <!-- Employee login
+ 
 Admin Login -->
+
+<form action="" method="post">
+    User Name: <input type="text"  name="adminID" required>
+    <br>
+    Password: <input type="text" name="password" required>
+    <br>
+    </form>
 
 </body>
 </html>
