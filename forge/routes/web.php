2513b31bf9898840c6c7013f7c8bd4bf4777da1e
iallison0231@stevenscollege.edu <isaacallison562@gmail.com>
2025-11-05 02:22:31 -0500

    Edit employee

--- forge/routes/web.php
+++ forge/routes/web.php
@@ -17,3 +17,7 @@ Route::post('/test', [EmployeeController::class, 'employeeForm' ]);
 Route::get('/signinPage', [EmployeeController::class, 'signinPage']);
 Route::post('/employeeProfile', [EmployeeController::class, 'login'])->name('employee.login');
 Route::get('/employeeProfile', [EmployeeController::class, 'employeeProfile'])->name('employee.profile');
+
+//Edit employee
+Route::get('/editEmployee', [EmployeeController::class, 'viewEditEmployee']);
+Route::post('/editEmployee', [EmployeeController::class, 'editEmployee']);
